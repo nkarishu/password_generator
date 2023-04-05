a2f39fbcff1d11ff4151b53e55db75454e5e8848
nkarishu <86212360+nkarishu@users.noreply.github.com>
2023-04-05 10:37:39 +1000

    up

--- index.js
+++ index.js
@@ -37,6 +37,5 @@ function showPasswords(){
 //copy password
 function copyPassword() {
     navigator.clipboard.writeText(generateGlobal.value);
-    // copyText.textContent = copyDefault;
-    alert("Copied the text: " + generateGlobal.value);
+    alert("Copied the text: " + generateGlobal);
 }
